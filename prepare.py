34ac459feed915615bb4294a66917ad08193cc3b
Alberto Puentes <albertopuentes00@gmail.com>
2021-05-20 21:24:21 -0500

    Prepare Functions - prepare Titanic added

--- prepare.py
+++ prepare.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 # train test split from sklearn
 from sklearn.model_selection import train_test_split
 # imputer from sklearn
@@ -39,3 +39,23 @@ def prep_iris(df):
     # Combine my dfs to form a final cleaned version
     clean_iris_df = pd.concat([df_iris_droppedr, dummy_df], axis=1)
     return clean_iris_df
+
+def prep_titanic(df):
+    ‘’'
+    This function take in the titanic data acquired by get_titanic_data,
+    Returns prepped train, validate, and test dfs with embarked dummy vars,
+    deck dropped, and the mean of age imputed for Null values.
+    ‘’'
+    # drop rows where embarked/embark town are null values
+    df = df[~df.embarked.isnull()]
+    # encode embarked using dummy columns
+    titanic_dummies = pd.get_dummies(df.embarked, drop_first=True)
+    # join dummy columns back to df
+    df = pd.concat([df, titanic_dummies], axis=1)
+    # drop the deck column
+    df = df.drop(columns=‘deck’)
+    # split data into train, validate, test dfs
+    train, validate, test = titanic_split(df)
+    # impute mean of age into null values in age column
+    train, validate, test = impute_mean_age(train, validate, test)
+    return train, validate, test
